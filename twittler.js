1c5f0e6c160b27b3090c98992e00d18c84eb9124
Max Kroshka <max.kroshka@gmail.com>
2015-12-03 15:13:15 -0800

    Added refresh function

--- twittler.js
+++ twittler.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var $timeline = $('.timeline');
+  var historyLength = streams.home.length;
 
   var tweetAppend = function(index) {
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.text(tweet.created_at+' @' + tweet.user + ': ' + tweet.message);
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + " " + jQuery.timeago(tweet.created_at));
     $tweet.appendTo($timeline);
   }
-var initialLoad = function(){
+  var initialLoad = function() {
     $timeline.html();
     var index = streams.home.length - 1;
     while (index >= 0) {
@@ -15,8 +16,20 @@ var initialLoad = function(){
       index -= 1;
     }
   }
-initialLoad();
+  initialLoad();
 
-// $(".refresh").on('click',refresh());
-
+  var refresh = function(){
+    var currentLength = streams.home.length;
+    console.log(currentLength);
+    if(currentLength > historyLength){
+      var difference = currentLength - historyLength;
+      for(var i=0;i<difference;i++){
+        tweetAppend(i);
+      }
+      historyLength = currentLength;
+    }
+  }
+  $(".refresh").click(function(){
+    refresh();
+  });
 });
